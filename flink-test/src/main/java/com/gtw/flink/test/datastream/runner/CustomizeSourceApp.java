e86690f5bda875a2ccd286d0080ea9ba2df6e00a
gaotingwang <code_butcher@sina.com>
2024-02-23 22:06:06 +0800

    Flink-自定义Sink到文件系统

--- flink-test/src/main/java/com/gtw/flink/test/datastream/runner/CustomizeSourceApp.java
+++ flink-test/src/main/java/com/gtw/flink/test/datastream/runner/CustomizeSourceApp.java
@@ -5,12 +5,19 @@ import com.gtw.flink.test.datastream.partition.AccessPartitioner;
 import com.gtw.flink.test.datastream.sink.ConsoleSinkFunction;
 import com.gtw.flink.test.datastream.source.ParallelAccessSource;
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
+
+import java.time.Duration;
 
 /**
  * 采用批流一体的处理方式
@@ -20,6 +27,7 @@ public class CustomizeSourceApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
+        env.enableCheckpointing(300);
 
 //        DataStreamSource<Access> source = env.addSource(new AccessSource());
         DataStreamSource<Access> source = env.addSource(new ParallelAccessSource());
@@ -53,7 +61,13 @@ public class CustomizeSourceApp {
 
         // 从指定tag中获取
         processSource.addSink(new ConsoleSinkFunction());
-        processSource.getSideOutput(outputTag1).print("1分流");
+        processSource.getSideOutput(outputTag1).addSink(StreamingFileSink.<Access>forRowFormat(new Path("out"), new SimpleStringEncoder<>())
+                .withRollingPolicy(DefaultRollingPolicy.builder() // 文件滚动策略
+                        .withRolloverInterval(Duration.ofMinutes(15)) // 按时间间隔滚
+                        .withInactivityInterval(Duration.ofMinutes(5)) // 按不活跃间隔
+                        .withMaxPartSize(MemorySize.ofMebiBytes(1024)) // 按大小滚
+                        .build())
+                .build());
         processSource.getSideOutput(outputTag2).print("2分流");
 
         env.execute("作业名字");
